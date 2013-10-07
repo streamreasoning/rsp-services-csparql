ef0303b355f3a6a13e2003bc82392798efa90278
Marco Balduini <balduini.marco@gmail.com>
2013-10-07 11:08:15 +0200

    - lincense header updated - repository changed in pom.xml

--- src/main/java/polimi/deib/rsp_service4csparql_server/query/MultipleQueriesDataServer.java
+++ src/main/java/polimi/deib/rsp_service4csparql_server/query/MultipleQueriesDataServer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2013 DEIB - Politecnico di Milano
+ * Copyright 2013 Marco Balduini, Emanuele Della Valle
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Acknowledgements:
+ * 
+ * This work was partially supported by the European project LarKC (FP7-215535) 
+ * and by the European project MODAClouds (FP7-318484)
  ******************************************************************************/
 package polimi.deib.rsp_service4csparql_server.query;
 
